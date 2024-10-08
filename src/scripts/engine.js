5238badb0ec6f4bfa551abf29b89fe9c85a47558
vandorocha <vanderlandio.zr@gmail.com>
2024-10-08 20:27:36 -0300

    Implementado captura de teclas e ajuste de volume

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,13 +1,37 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
 
+let mapedKeys = [];
 let audio = new Audio("src/tunes/a.wav")
 
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`
     audio.play();
-}
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    },150);
+};
 
 pianoKeys.forEach((key)=>{
     console.log(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key));
-})
+    mapedKeys.push(key.dataset.key);
+});
+
+
+document.addEventListener("keydown", (e) =>{
+
+    if(mapedKeys.includes(e.key)){
+        playTune(e.key);
+    }
+});
+
+const handleVolume = (e) =>{
+    audio.volume = e.target.value;
+    console.log(e.target.value);
+}
+
+volumeSlider.addEventListener("input", handleVolume)
